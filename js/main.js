27c73dec7a3679b4d44e862ae6d01f4194d73c72
YarTretyak <73073530+YarTretyak@users.noreply.github.com>
2021-04-07 18:10:09 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+VANTA.NET({
+  el: "#animation",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xff6800,
+  backgroundColor: 0x180c2d
+})
